bcff78e84446aa4931f3d396a8caa9b766f78088
RobertTheDev <robertthedev@gmail.com>
2023-10-23 17:23:38 +0100

    feat: created first test for header

---
+++ components/__tests__/Header.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import Header from "../Header/index.vue";
+
+describe("HelloWorld", () => {
+  it("Header component is a Vue instance", () => {
+    const wrapper = mount(Header);
+    expect(wrapper.vm).toBeTruthy();
+  });
+});
