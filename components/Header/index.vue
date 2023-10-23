c15553b3243c188b5f3f01e579aa7d1336dd8c15
RobertTheDev <robertthedev@gmail.com>
2023-10-23 13:35:42 +0100

    feat: created header component

---
+++ components/Header/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="flex h-16 justify-center gap-4 shadow-md items-center">
+    <div>
+      <h1>Logo</h1>
+    </div>
+    <div>
+      <nav class="flex gap-4">
+        <NuxtLink
+          v-for="navigationLink in navigationLinks"
+          :to="navigationLink.url"
+          >{{ navigationLink.name }}</NuxtLink
+        >
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+const navigationLinks = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Profile",
+    url: "/profile",
+  },
+];
+</script>
