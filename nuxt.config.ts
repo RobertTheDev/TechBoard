6cee2c0ad3393163bb74bf4bef35d670ee29b6f5
RobertTheDev <robertthedev@gmail.com>
2023-10-23 17:58:00 +0100

    feat: installed and configured prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
-import companyName from "./lib/constants/companyName";
+import companyName from './lib/constants/companyName'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss'],
   app: {
     head: {
       titleTemplate: `%s - ${companyName}`,
-      charset: "utf-8",
+      charset: 'utf-8',
       meta: [
         {
-          name: "description",
+          name: 'description',
           content:
-            "TechBoard is an online marketplace for finding tech jobs online. This is not a commercial website and is part of a portfolio developed by Robert the Dev.",
+            'TechBoard is an online marketplace for finding tech jobs online. This is not a commercial website and is part of a portfolio developed by Robert the Dev.',
         },
       ],
     },
   },
-});
+})
