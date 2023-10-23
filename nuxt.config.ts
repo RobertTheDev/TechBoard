1e0c943be8e2954742caad1bad9fabaa636d844c
RobertTheDev <robertthedev@gmail.com>
2023-10-23 14:21:03 +0100

    feat: set up seo config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+import companyName from "./lib/constants/companyName";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      titleTemplate: `%s - ${companyName}`,
+      charset: "utf-8",
+      meta: [
+        {
+          name: "description",
+          content:
+            "TechBoard is an online marketplace for finding tech jobs online. This is not a commercial website and is part of a portfolio developed by Robert the Dev.",
+        },
+      ],
+    },
+  },
 });
