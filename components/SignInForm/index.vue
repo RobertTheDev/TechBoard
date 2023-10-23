31a84d011f89edaec08bf7899236c29d44fa085c
RobertTheDev <robertthedev@gmail.com>
2023-10-23 18:37:01 +0100

    feat: added sign in schema

--- components/SignInForm/index.vue
+++ components/SignInForm/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <form>
-      <input type="email" name="email" />
-      <button>Sign In</button>
-    </form>
-  </div>
+  <Form :validation-schema="signInSchema" @submit="handleSignUp">
+    <Field type="email" name="emailAddress" />
+    <ErrorMessage name="emailAddress" />
+    <button type="submit">Sign In</button>
+  </Form>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { toTypedSchema } from '@vee-validate/zod'
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import signInValidationSchema from '~/models/auth/validators/signUp.schema'
+
+const signInSchema = toTypedSchema(signInValidationSchema)
+
+function handleSignUp(values: unknown) {
+  alert(JSON.stringify(values))
+}
+</script>
