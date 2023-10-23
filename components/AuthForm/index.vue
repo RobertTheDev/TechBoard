c960de2f92a2acf71d0e6a9670b63a6ded0a18e6
RobertTheDev <robertthedev@gmail.com>
2023-10-23 18:15:59 +0100

    feat: created auth form component

---
+++ components/AuthForm/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="handleFormType('signIn')">Sign In</button>
+    <button @click="handleFormType('signUp')">Sign Up</button>
+    <SignInForm v-if="formType === 'signIn'" />
+    <SignUpForm v-if="formType === 'signUp'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const formType = ref('signIn')
+
+function handleFormType(type: string) {
+  formType.value = type
+}
+</script>
